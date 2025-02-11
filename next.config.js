f77a86670bea1d20a84b91030ffbd2c0a9d683c1
Eric Cheni <sweetdream0828@gmail.com>
2025-02-11 08:37:10 -0800

    Updated by sir

--- next.config.js
+++ next.config.js
@@ -1,35 +1,35 @@
 /** @type {import('next').NextConfig} */
 
-const { i18n } = require('./next-i18next.config');
+const { i18n } = require("./next-i18next.config");
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   compiler: {
     // Enables the styled-components SWC transform
-    styledComponents: true
+    styledComponents: true,
   },
   audio: {
-    modules: ['mp3', 'aac', 'wav', 'ogg', 'm4a', 'flac'],
+    modules: ["mp3", "aac", "wav", "ogg", "m4a", "flac"],
   },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'i.imgur.com',
-      }
-    ]
+        protocol: "https",
+        hostname: "i.imgur.com",
+      },
+    ],
+    domains: ["rainydreams.vercel.app"],
   },
   webpack: (config, { isServer }) => {
     if (!isServer) {
-      config.resolve.fallback.fs = false
+      config.resolve.fallback.fs = false;
     }
-    return config
+    return config;
   },
-}
-
+};
 
 module.exports = {
   i18n,
-  nextConfig
+  nextConfig,
 };
